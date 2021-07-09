831bcfb7bca670ebb7a06bad93e05c3effeb4509
Tabisch <tobithieman@gmail.com>
2021-07-09 17:22:49 +0200

    JoyStick Implementaion V0.1
    Something is broken, but data gets transfert

---
+++ Xinput_Xoutput/Xinput_Xoutput.ino
@@ -0,0 +1,139 @@
+#include <XInput.h>
+
+String dataIn;
+short selectionshort;
+short messageshort;
+short stickXValue;
+bool stickXvalueSet;
+bool desynced = false;
+
+void setup() {
+  Serial1.begin(9600);
+  XInput.begin();
+}
+
+void handleDesync()
+{
+  dataIn = "";
+  messageshort = 0;
+  selectionshort = 0;
+  stickXValue = 0;
+  stickXvalueSet = false;
+  desynced = true;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readDataFromSerial1();
+
+  if(desynced)
+  {
+    desynced = false;
+    return;
+  }
+
+  if(selectionshort == -1)
+  {
+    Serial1.print("Keep Alive");
+    Serial1.write('\n');
+    return;
+  }
+
+  if(selectionshort < 15)
+  {
+    handleButtons();
+  }
+  
+  if(selectionshort == 15 || selectionshort == 16)
+  {
+    
+  }
+
+  if(selectionshort == 17 || selectionshort == 18)
+  {
+    handleJoySticks();
+  }
+  
+  Serial1.print(String(selectionshort));
+  Serial1.write('\n');
+}
+
+void readDataFromSerial1()
+{
+  unsigned long StartTime = millis();
+  
+  boolean readFirst = true;
+  
+  while(true)
+  { 
+    if(millis() - StartTime >= 1000)
+    {
+      Serial1.print("Desync");
+      Serial1.write('\n');
+
+      handleDesync();
+      return;
+    }
+    
+    if(!Serial1.available())
+    {
+      continue;
+    }
+    
+    char temp = Serial1.read();
+
+    if(temp == '\n')
+    {
+      if(readFirst)
+      {
+        readFirst = false;
+        selectionshort = dataIn.toInt();
+        dataIn = "";
+        StartTime = millis();
+      }
+      else
+      {
+        messageshort = dataIn.toInt();
+        dataIn = "";
+        return;
+      }
+    }
+
+    dataIn += temp;
+  }
+}
+
+void handleButtons()
+{
+  if(messageshort == 1)
+  {
+    XInput.press(XInputControl(selectionshort));
+  }
+  else
+  {
+    XInput.release(XInputControl(selectionshort));
+  }
+}
+
+void handleJoySticks()
+{
+  if(stickXvalueSet)
+  {
+    if(selectionshort == 17)
+    {
+      XInput.setJoystick(JOY_LEFT,stickXValue, messageshort);
+    }
+    else
+    {
+      XInput.setJoystick(JOY_RIGHT,stickXValue, messageshort);
+    }
+
+    stickXvalueSet = false;
+    messageshort = 0 
+  }
+  else
+  {
+    stickXvalueSet = true;
+    stickXValue = messageshort;
+  }
+}
